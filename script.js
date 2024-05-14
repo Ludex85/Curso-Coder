d0df4266df3995ea7b2419dfeb44cadb6bdd4d25
Ludex85 <maximiliano_capello@hotmail.com>
2024-05-14 12:13:34 -0300

    fixes :)

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 window.addEventListener('resize', function() {
     // Obtener el contenedor del elemento <a>
-    var container = document.getElementById('twitter-container');
+    var container = document.getElementById('twitter-timeline');
 
     // Obtener la línea de tiempo de Twitter (iframe)
-    var twitterTimeline = container.querySelector('iframe');
+    var twittertimeline = container.querySelector('iframe');
 
     // Obtener la URL de la línea de tiempo
-    var twitterUrl = twitterTimeline.getAttribute('src');
+    var twitterUrl = twittertimeline.getAttribute('src');
 
     // Actualizar el src del iframe para forzar la recarga
-    twitterTimeline.src = '';
-    twitterTimeline.src = twitterUrl;
+    twittertimeline.src = '';
+    twittertimeline.src = twitterUrl;
 });
